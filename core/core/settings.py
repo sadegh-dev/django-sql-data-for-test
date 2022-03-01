9c0fb0974f06feed8040fa9ec5fa51826955429f
sadegh-dev <sadegh.rez@gmail.com>
2022-03-01 14:43:21 +0330

    edit settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFIELS_DIRS = os.path.join(BASE_DIR, 'static/') ,
+
+
+# Media
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
